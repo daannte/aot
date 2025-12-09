e8ad782d9f2e49bcaaaac0fb44f4a802e19f3d0b
Dante <github.6p8ng@kironmail.com>
2025-12-09 11:59:21 -0700

    refactor: part1 small refactor

--- year2025/day03.py
+++ year2025/day03.py
@@ -6,7 +6,6 @@ def right_side_maxima(digits: list[int]) -> list[int]:
     n = len(digits)
     res = [0] * n
     max_so_far = -1
-
     for i in range(n - 1, -1, -1):
         res[i] = max_so_far
         max_so_far = max(max_so_far, digits[i])
@@ -18,9 +17,6 @@ def max_jolt_for_line(digits: list[int]) -> int:
     right_max = right_side_maxima(digits)
     best = -1
     for i, d in enumerate(digits[:-1]):
-        if right_max[i] == -1:
-            continue
-
         jolt = d * 10 + right_max[i]
         best = max(best, jolt)
 
@@ -32,6 +28,7 @@ def part1(lines: list[str]) -> int:
     for line in lines:
         digits = list(map(int, line.strip()))
         total += max_jolt_for_line(digits)
+
     return total
 
 
